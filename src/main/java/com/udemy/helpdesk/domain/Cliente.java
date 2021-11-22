f99a2e66ca724daa52909de99f88efe875835b35
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-11-22 13:47:34 -0300

    Anotações JPA para classes de dominio.

--- src/main/java/com/udemy/helpdesk/domain/Cliente.java
+++ src/main/java/com/udemy/helpdesk/domain/Cliente.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.udemy.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List <Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/udemy/helpdesk/domain/Tecnico.java
+++ src/main/java/com/udemy/helpdesk/domain/Tecnico.java
@@ -3,16 +3,26 @@ package com.udemy.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.udemy.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Tecnico extends Pessoa{
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "tecnico")
 	private List <Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
