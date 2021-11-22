39eda3327c02ddf7fa5cd11f15d672eda156fbff
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-11-22 20:57:59 -0300

    Protegendo aplicação contra serealização

--- src/main/java/com/udemy/helpdesk/domain/Cliente.java
+++ src/main/java/com/udemy/helpdesk/domain/Cliente.java
@@ -7,12 +7,14 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.udemy.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List <Chamado> chamados = new ArrayList<>();
 
