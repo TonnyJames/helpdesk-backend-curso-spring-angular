2c3c1dc90268ff1bc293789408994bd3c98e7456
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-12-10 14:04:09 -0300

    Validação dos campos de criação de novo usuario

--- src/main/java/com/udemy/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -32,6 +34,22 @@ public class ResourceExceptionHandler {
 				"Violação de dados", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+				
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, 
+			HttpServletRequest request){
+		
+
+		ValidationError  errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Validation Error", "Erro na Validação dos campos" , request.getRequestURI());
+		
+		for(FieldError x: ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 				
 	}
 }
